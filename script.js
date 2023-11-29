3e89d82661e84f415876c22722020f0e6e881821
moonleeks <moonleeks@proton.me>
2023-11-29 17:19:36 +0200

    more worki
    moved digit buttons to their own container, display kinda updates and
    clears now

--- script.js
+++ script.js
@@ -23,7 +23,48 @@ function operate(a, b, operator) {
     }
 }
 
-let a = 0;
-let b = 0;
+function updateDisplay() {
+    console.log("updating display");
+    display.textContent = a + " " + operator + " " + b;
+
+}
+
+function clear() {
+    console.log("clearing");
+    display.textContent = "";
+    a = 'x';
+    b = 'x';
+    operator = 'x';
+}
+
+
+let a = 'x';
+let b = 'x';
 let operator = 'x';
+const display = document.querySelector('.display');
+clear();
+
+const buttonsContainer = document.querySelector('.buttonsContainer');
+buttonsContainer.addEventListener('click', (e) => {
+    if (e.target.parentNode.classList.contains('digitContainer')) {
+        if (a === 'x') {
+            a = parseInt(e.target.dataset.symbol);
+        } else {
+            b = parseInt(e.target.dataset.symbol);
+        }
+        console.log("digit pressed");
+        updateDisplay();
+    } else {
+        switch (e.target.id) {
+            case 'add': operator = '+';
+            case 'substract': operator = '-';
+            case 'multiply': operator = '*';
+            case 'divide': operator = '/';
+            case 'calculate': operate(a, b, operator);
+            case 'clear': clear();
+        }
+        updateDisplay();
+        console.log("nondigit pressed");
+    }
+});
 
